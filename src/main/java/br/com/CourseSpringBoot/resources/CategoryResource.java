67dc6c73ca9417dc855e4fb9dc3ecfaa0b5880d6
Fabricio Caires <fabricio.vieira995@hotmail.com>
2019-01-10 01:05:34 -0200

    Test first domain class

--- src/main/java/br/com/CourseSpringBoot/resources/CategoryResource.java
+++ src/main/java/br/com/CourseSpringBoot/resources/CategoryResource.java
@@ -1,8 +1,12 @@
 package br.com.CourseSpringBoot.resources;
 
+import br.com.CourseSpringBoot.domain.Category;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author fabricio
@@ -13,7 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryResource {
 
     @GetMapping("/list")
-    public String listar(){
-        return "SPRING IS WORKING AND DOING WELL";
+    public List<Category> listar(){
+
+        Category c1 = new Category(1, "Informática");
+        Category c2 = new Category(2, "Escritório");
+
+        List<Category> list = new ArrayList<>();
+        list.add(c1);
+        list.add(c2);
+
+        return list;
     }
 }
